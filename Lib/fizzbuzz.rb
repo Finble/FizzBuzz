8afd423cbee231b2f3b3d0e2ded3d26efe3cd259
Finble <elainefinn@hotmail.com>
2016-01-15 12:49:19 +0000

    final fizzbuzz.rb file

--- Lib/fizzbuzz.rb
+++ Lib/fizzbuzz.rb
@@ -1,17 +1,18 @@
-def fizzbuzz(number)
+class Integer
 
+	def fizzbuzz
 
-	if number %3 == 0 and number %5 == 0
-		return 'fizzbuzz'
-	elsif number %3 == 0
-		return 'fizz'
-	elsif number %5  == 0
-		return 'buzz'
-	else
-		return number
+		if self %3 == 0 and self %5 == 0
+			return 'fizzbuzz'
+		elsif self %3 == 0
+			return 'fizz'
+		elsif self %5  == 0
+			return 'buzz'
+		else
+			return self
+		end
 	end
 end
-
 
 # tests 1-3
 
@@ -24,3 +25,34 @@ end
 # 		return 'fizzbuzz'
 # 	end
 # end
+
+# second pair
+
+# def fizzbuzz(number)
+#   if number % 15 == 0
+#     return 'fizzbuzz'
+#   elsif number % 3 == 0
+#     return 'fizz'
+#   elsif number % 5 == 0
+#     return 'buzz'
+#   else
+#     number
+#   end
+# end
+
+# class Integer
+
+# 	def fizzbuzz
+
+# 		if self %3 == 0 and self %5 == 0
+# 			return 'fizzbuzz'
+# 		elsif self %3 == 0
+# 			return 'fizz'
+# 		elsif self %5  == 0
+# 			return 'buzz'
+# 		else
+# 			return self
+# 		end
+# 	end
+# end
+
